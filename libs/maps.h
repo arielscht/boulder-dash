4e20dd1669282663942618e45f4a0f8d3aa5014f
Ariel Evaldt Schmitt <48089807+arielscht@users.noreply.github.com>
2021-11-08 23:07:38 -0300

    map1.txt

---
+++ libs/maps.h
@@ -0,0 +1,15 @@
+#define MAP_ROCKFORD 'R'
+#define MAP_DIRT 'I'
+#define MAP_BOULDER 'B'
+#define MAP_DIAMOND 'D'
+#define MAP_WALL 'W'
+#define MAP_STEEL_WALL 'S'
+#define MAP_MAGIC_WALL 'M'
+#define MAP_FIREFLY 'F'
+#define MAP_BUTTERFLY 'Y'
+#define MAP_AMOEBA 'A'
+#define MAP_EXIT 'E'
+#define MAP_BLANK 'O'
+
+#define MAP_WIDTH 40
+#define MAP_HEIGHT 22
